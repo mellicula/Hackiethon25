6c359deb475bdb8c6b9a67c08f0e0f97c3be93eb
Honey Raut <hraut.au@gmail.com>
2025-03-29 01:06:28 +1100

    upload started

--- showcase-site/src/submission-widgets/MyWidget.jsx
+++ showcase-site/src/submission-widgets/MyWidget.jsx
@@ -1,26 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
+import ICAL from "ical.js";
+import axios from "axios";
+
 
 const MyWidget = () => {
-  const [text, setText] = useState('Hello, World!');
+  const [ file, setFile ] = useState(null);
+  const [ progress, setProgress ] = useState({started: false, pc: 0});
+  const [ msg, setMsg ] = useState("");
 
-  const changeText = () => setText('Text has been changed!');
+  function handleUpload() {
+    if (!file) {
+      setMsg("no file selected");
+      return;
+    }
+    const fd = new FormData();
+    fd.append('file', file);
+
+    setMsg("Uploading...");
+    setProgress(prevState => {
+      return {...prevState, started:true}
+    })
+    axios.post('http://httpbin.org/post', fd, {
+      onUploadProgress: (progressEvent) => { setProgress(prevState => { 
+      return {...prevState, pc: progressEvent.progress*100}
+      }) },
+      headers: {
+        "Custom-Header": "value",
+      }
+    })
+    .then(res => {
+      setMsg("Upload successfull");
+      console.log(res.data);
+    })
+    .catch(err => {
+      setMsg("Error");
+      console.error(err);
+    });
+  }
+  //console.log(mycal);
 
   return (
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg">
       <div className="text-center space-y-4">
-        <h2 className="text-xl font-bold text-gray-800">Empty Component</h2>
+        <h2 className="text-xl font-bold text-gray-800">Upload file</h2>
 
         <div className="text-2xl font-bold text-blue-600">
-          {text}
+          {msg}
         </div>
 
         <div className="flex justify-center">
-          <button
-            onClick={changeText}
-            className="p-2 bg-blue-100 rounded-full hover:bg-blue-200 transition-colors"
-          >
-            Change Text
+          <input onChange ={(e) => {setFile(e.target.files[0])} } type="file"/>
+          <button onClick={handleUpload} className="p-2 bg-blue-100 rounded-full hover:bg-blue-200 transition-colors">
+            Upload ICS file
           </button>
+          {progress.started && <progress max ="100" value={progress.pc}></progress> }
         </div>
       </div>
     </div>
@@ -28,3 +61,5 @@ const MyWidget = () => {
 };
 
 export default MyWidget;
+
+
